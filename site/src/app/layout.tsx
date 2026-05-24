87b011a2c91921b42e033adb620b6d5286557705
Jeet Ajmani <67179741+jeetajmani@users.noreply.github.com>
2026-05-24 01:14:35 -0700

    uncomment layout

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type React from "react"
-// import "@/app/styles/globals.css"
+import "@/app/styles/globals.css"
 import { Inter } from "next/font/google"
 import type { Metadata } from "next"
 
